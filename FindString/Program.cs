903b81833edaf1835d1ecf7cc134928792916693
Juliano <fehlipe.contato@gmail.com>
2024-10-30 07:40:36 -0300

    commit

---
+++ FindString/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Digite alguma palavara: ");
+string? String = Console.ReadLine();
+char letra = 'a';
+int Cont = 0;
+
+if (String.ToLower().Contains(letra))
+{
+    foreach (var i in String)
+    {
+        if (i == letra)
+        {
+            Cont++;
+        }
+    }
+    Console.WriteLine($"A letra 'a' aparece {Cont} vezes na string {String}.");
+}
+else
+{
+    Console.WriteLine($"A letra 'a' não aparece na string {String}");
+}
+
---
+++ desafio-tecnico-targetsistemas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Xml;
+
+Console.WriteLine("Digite um inteiro: ");
+int Inteiro = Convert.ToInt32(Console.ReadLine());
+
+List<int> sequencia = new List<int> { 0, 1 };
+
+bool Continua = true;
+
+while (Continua)
+{
+    if (sequencia.Contains(Inteiro))
+    {
+        Console.WriteLine($"O valor {Inteiro} faz parte da sequencia de Fibonacci.");
+        Continua = false;
+    }
+
+    int UltimoValorDaLista = sequencia.Last(); 
+    int PenultimoValor = sequencia[sequencia.Count - 2];
+    var NovoValor = UltimoValorDaLista + PenultimoValor;
+
+    if (NovoValor == Inteiro)
+    {
+        Console.WriteLine($"O valor {NovoValor} faz parte da sequencia de Fibonacci.");
+        Continua = false;
+    }else if (NovoValor < Inteiro)
+    {
+        sequencia.Add(NovoValor);
+    }
+    else
+    {
+        Console.WriteLine($"O valor {Inteiro} não faz parte da sequencia de Fibonacci.");
+        Continua = false;
+    }
+}
+
+
